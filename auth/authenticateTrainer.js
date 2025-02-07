46dabd07900bcf5e9637abaa4e24c201a076d116
MaryLizR <marylizr@gmail.com>
2025-02-07 18:04:54 +0200

    Fix tracking issue by clearing cache

--- auth/authenticateTrainer.js
+++ auth/authenticateTrainer.js
@@ -1,8 +1,8 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/userModel'); // Adjust the path as needed
+const User = require('../models/userModel'); 
 
 const authenticateTrainer = async (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1]; // Extract token from `Authorization` header
+  const token = req.headers.authorization?.split(' ')[1]; 
 
   if (!token) {
     return res.status(401).json({ message: "Unauthorized - No token provided" });
