55690e3ce4eff696322d0fdd21b8f331d4a778e9
MaryLizR <marylizr@gmail.com>
2025-02-10 18:46:17 +0200

    model

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/userModel'); 
+const User = require('../models/userModel.js'); 
 
 exports.login = async (req, res) => {
   const { email, password } = req.body;
--- router/loginRouter.js
+++ router/loginRouter.js
@@ -9,9 +9,3 @@ loginRouter.post('/login', loginController.login);
 
 module.exports = loginRouter;
 
-res.cookie('token', token, {
-   httpOnly: true,  // Prevents JavaScript from accessing the token
-   secure: true,    // Ensures it's sent only over HTTPS
-   sameSite: 'None' // Required for cross-origin requests (important for Netlify + Heroku)
- });
- 
