0b59beb3736b8d0a2d76ad25cd1722f242bb2d27
MaryLizR <marylizr@gmail.com>
2025-11-04 19:38:50 +0100

    addworkout finally works

--- models/addWorkoutModel.js
+++ models/addWorkoutModel.js
@@ -1,18 +1,55 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
+const mongoose = require("mongoose");
 
-const addWorkSchema = new Schema({
-  type: String,
-  workoutName: String,
-  description: String,
-  reps: Number,
-  series: Number,
-  lifted: Number,
-  picture: String,
-  video: String,
-});
+const addWorkoutSchema = new mongoose.Schema(
+  {
+    type: {
+      type: String,
+      required: false,
+    },
+    workoutName: {
+      type: String,
+      required: false,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    reps: {
+      type: String,
+      required: false,
+    },
+    series: {
+      type: String,
+      required: false,
+    },
+    picture: {
+      type: String,
+      required: false,
+    },
+    video: {
+      type: String,
+      required: false,
+    },
+    workoutLevel: {
+      type: String,
+      enum: ["basic", "medium", "advanced"],
+      required: false,
+    },
+    trainerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: false,
+    },
+    isGeneral: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const AddWork = mongoose.model("addwork", addWorkSchema);
+const AddWork = mongoose.model("AddWork", addWorkoutSchema);
 
 module.exports = AddWork;
